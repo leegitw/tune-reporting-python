00f324b09b4e58946f37a6fce90553432a234a38
Jeff Tanner <jefft@tune.com>
2017-03-14 15:57:38 +0200

    Update __init__.py

--- tune_reporting/__init__.py
+++ tune_reporting/__init__.py
@@ -4,8 +4,8 @@
 #  @namespace tune_reporting
 
 __title__ = 'tune-reporting'
-__version__ = '2.1.8'
-__build__ = 0x020108
+__version__ = '2.1.9'
+__build__ = 0x020109
 __version_info__ = tuple(__version__.split('.'))
 
 __author__ = 'jefft@tune.com'
